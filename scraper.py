915a6f4c70f32d0b449e23aa01bfebeac8b2999a
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-10-30 20:18:11 -0400

    removed scanning subsubdirectories, fixed the issue with repeating subsubdirectories, and added progress updates

--- scraper.py
+++ scraper.py
@@ -55,91 +55,68 @@ for section in all_sections:
         # Get & make the directory for the section
         section_dir = f"{ builds_dir }/{ subsuburl }"
         safe_mkdir(section_dir)
+        print(f"Started scraping section { subsuburl }!")
         continue
     
     # Get & make the directory for the subsection
     subsection_dir = f"{ section_dir }/{ subsuburl }"
     safe_mkdir(subsection_dir)
+    print(f"\tStarted scraping subsection { subsuburl }!")
 
     # Get the subdirectory's page
     suburl = f"{ site_link }/minecraft/{ subsuburl }"
     subsection_res = requests.get(suburl).text
-    # Get the subsubsection table
-    sub2section_table_header = "<ul class=\"menu level_4" # Inspect element shows it as level_3, but it's level_4 when querying
-    sub2section_table_tail = "</ul>"
-    sub2section_table_start = subsection_res.find(sub2section_table_header)
-    sub2section_table_end = subsection_res.find(sub2section_table_tail, sub2section_table_start)
-    sub2section_table = subsection_res[sub2section_table_start:sub2section_table_end]
 
-    # Get the subsubsections
-    sub2sections = sub2section_table.split(section_header)
-    # Remove the first useless part
-    sub2sections.pop(0)
-    
-    #Get the subsubsections
-    for sub2section in sub2sections:
-        # Get the subdirectory name of the section for the website
-        sub2section_start = sub2section_table.find(suburl_header) + len(suburl_header)
-        sub2section_end = sub2section_table.find(suburl_tail, sub2section_start)
-        sub2suburl = sub2section_table[sub2section_start:sub2section_end]
+    # Get the number of pages of the subdirectory from the to last button
+    # Get the button
+    to_last_button_header = "<li class=\"last\">"
+    to_last_button_start = subsection_res.find(to_last_button_header)
 
-        # Get & make the directory for the subsubsection
-        sub2dir = f"{ subsection_dir }/{ sub2suburl }"
-        safe_mkdir(sub2dir)
+    # Get the number of pages
+    num_pages_header = "/pg/"
+    num_pages_start = subsection_res.find(num_pages_header, to_last_button_start) + len(num_pages_header)
+    num_pages_end = subsection_res.find(suburl_tail, num_pages_start)
+    num_pages = int(subsection_res[num_pages_start:num_pages_end])
 
-        # Get the subsubdirectory url
-        sub2url = f"{ site_link }/minecraft/{ sub2suburl }"
-        sub2section_res = requests.get(sub2url).text
+    for i in range(1, num_pages + 1):
+        # Get the page (We refetch the first page just in case grabcraft changes how they display things by default)
+        subpage_url = f"{ suburl }/pg/{ i }"
+        subpage_res = requests.get(subpage_url).text
 
-        # Get the number of pages of the subsubdirectory from the to last button
-        # Get the button
-        to_last_button_header = "<li class=\"last\">"
-        to_last_button_start = sub2section_res.find(to_last_button_header)
+        # Get the build's image and then get the link from that
+        # Get the image
+        image_header = "<img src=\""
+        subpage_split_image = subpage_res.split(image_header)
+        subpage_split_image.pop(len(subpage_split_image) - 1)
+        # Extract the link from the image
+        for subpage_split in subpage_split_image:
+            # Get the build's link
+            a_href_header = "<a href=\""
+            build_link_start = subpage_split.rfind(a_href_header) + len(a_href_header)
+            build_link_end = subpage_split.find(suburl_tail, build_link_start)
+            build_sub_link = subpage_split[build_link_start:build_link_end]
+            build_link = f"{ site_link }{ build_sub_link }"
+            # Get the build's link name
+            build_link_name_header = '/minecraft/'
+            build_link_name_tail = '/'
+            build_link_name_start = build_sub_link.find(build_link_name_header) + len(build_link_name_header)
+            build_link_name_end = build_sub_link.find(build_link_name_tail, build_link_name_start)
+            build_link_name = build_sub_link[build_link_name_start:build_link_name_end]
+            
+            # Create the directory for containing the build
+            build_dir = f"{ subsection_dir }/{ build_link_name }"
+            safe_mkdir(build_dir)
+            print(f"\t\tStarted scraping build { build_link_name }!")
 
-        # Get the number of pages
-        num_pages_header = "/pg/"
-        num_pages_start = sub2section_res.find(num_pages_header, to_last_button_start) + len(num_pages_header)
-        num_pages_end = sub2section_res.find(suburl_tail, num_pages_start)
-        num_pages = int(sub2section_res[num_pages_start:num_pages_end])
-
-        for i in range(1, num_pages + 1):
-            # Get the page (We refetch the first page just in case grabcraft changes how they display things by default)
-            sub2page_url = f"{ sub2url }/pg/{ i }"
-            sub2page_res = requests.get(sub2page_url).text
-
-            # Get the build's image and then get the link from that
-            # Get the image
-            image_header = "<img src=\""
-            sub2page_split_image = sub2page_res.split(image_header)
-            sub2page_split_image.pop(len(sub2page_split_image) - 1)
-            # Extract the link from the image
-            for sub2page_split in sub2page_split_image:
-                # Get the build's link
-                a_href_header = "<a href=\""
-                build_link_start = sub2page_split.rfind(a_href_header) + len(a_href_header)
-                build_link_end = sub2page_split.find(suburl_tail, build_link_start)
-                build_sub_link = sub2page_split[build_link_start:build_link_end]
-                build_link = f"{ site_link }{ build_sub_link }"
-                # Get the build's link name
-                build_link_name_header = '/minecraft/'
-                build_link_name_tail = '/'
-                build_link_name_start = build_sub_link.find(build_link_name_header) + len(build_link_name_header)
-                build_link_name_end = build_sub_link.find(build_link_name_tail, build_link_name_start)
-                build_link_name = build_sub_link[build_link_name_start:build_link_name_end]
-                
-                # Create the directory for containing the build
-                build_dir = f"{ sub2dir }/{ build_link_name }"
-                safe_mkdir(build_dir)
-
-                # Get the schema
-                render_object = gts.url_to_render_object_data(build_link)
-                # The RenderObject.obj converted from a javascript variable to a json string
-                ro_json = render_object.obj[render_object.obj.find('{'):]
-                # Write the RenderObject json
-                with open(f"{ build_dir }/data.json", 'w') as f:
-                    f.write(ro_json)
-                # Write the RenderObject's attributes/metadata as a txt formatted as a yml
-                with open(f"{ build_dir }/meta.yml", 'w') as f:
-                    f.write(f"name: { render_object.name }\n")
-                    f.write(f"dims: { list(render_object.dims) }\n")
-                    f.write(f"tags: { list(render_object.tags) }\n")
+            # Get the schema
+            render_object = gts.url_to_render_object_data(build_link)
+            # The RenderObject.obj converted from a javascript variable to a json string
+            ro_json = render_object.obj[render_object.obj.find('{'):]
+            # Write the RenderObject json
+            with open(f"{ build_dir }/data.json", 'w') as f:
+                f.write(ro_json)
+            # Write the RenderObject's attributes/metadata as a txt formatted as a yml
+            with open(f"{ build_dir }/meta.yml", 'w') as f:
+                f.write(f"name: { render_object.name }\n")
+                f.write(f"dims: { list(render_object.dims) }\n")
+                f.write(f"tags: { list(render_object.tags) }\n")
